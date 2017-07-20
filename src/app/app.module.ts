104c861bc2142ff024ceeec14f2ef7e2713c8150
elfprincexu <nicolas.xu89@gmail.com>
2017-07-20 15:27:31 +0800

    add jumbotron carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
 import { AppRoutingModule} from "./app.routing";
 import { SafePipe } from './utilitiy/safe.pipe'
 
 // 3rd party import
-import {BsDropdownModule} from "ngx-bootstrap"
+import {BsDropdownModule} from "ngx-bootstrap";
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
 @NgModule({
@@ -16,10 +18,13 @@ import {BsDropdownModule} from "ngx-bootstrap"
     AppComponent,
     VideoListComponent,
     VideoDetailComponent,
-    SafePipe
+    HomeComponent,
+    SafePipe,
   ],
   imports: [
+    // ngx-bootstrap
     BsDropdownModule.forRoot(),
+    CarouselModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
