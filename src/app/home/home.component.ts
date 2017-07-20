612a56f6bfb68761952f8502453014820b4fc65d
elfprincexu <nicolas.xu89@gmail.com>
2017-07-20 16:00:15 +0800

    add click event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  homeImageList=[
+    {
+      image: "assets/images/nature/4.jpg",
+      title: "image4",
+      content:"Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      link: "/videos/video4",
+    },
+    {
+      image: "assets/images/nature/5.jpg",
+      title: "image5",
+      content:"Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      link: "/videos/video5",
+    },
+    {
+      image: "assets/images/nature/6.jpg",
+      title: "image6",
+      content:"Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      link: "/videos/video6",
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  preventNormal(event: MouseEvent, image:any)
+  {
+    console.log(image)
+    event.preventDefault()
+    // alert("working...")
   }
 
 }
