97cd872bbb6a802b3e4f4285a8328d2349ed1267
elfprincexu <nicolas.xu89@gmail.com>
2017-07-20 13:26:47 +0800

    add dynamic routing

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute} from "@angular/router"
 
 @Component({
   selector: 'video-detail',
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.css']
 })
-export class VideoDetailComponent implements OnInit {
-
-  constructor() { }
+export class VideoDetailComponent implements OnInit, OnDestroy {
+  private routeSub:any;
+  slug: string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.routeSub = this.route.params.subscribe(params => {
+      console.log(params)
+      this.slug = params['slug']
+    })
+    // this.route.params.subscribe(function(params){
+    //   console.log(params)
+    // })
+  }
+  ngOnDestroy(){
+    this.routeSub.unsubscribe()
+
   }
 
 }
